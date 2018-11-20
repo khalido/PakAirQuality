0ee9c9853d0bc495d279cf9e8260e3f21fb9ed3d
Khalid Omar <khalid.omar@gmail.com>
2018-11-20 12:06:08 +0000

    changed hello route

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!' + "<h2>this is the slash route</h2>"
-
-#@app.route('/hello')
+#@app.route('/')
 #def hello_world():
-#    return "<h1>This is the /hello route</h1>" + '<p>Hello, World!</p>' 
+#    return 'Hello, World!' + "<h2>this is the slash route</h2>"
+
+@app.route('/hello')
+def hello_world():
+    return "<h1>This is the /hello route</h1>" + '<p>Hello, World!</p>' 
 
 
 if __name__ == '__main__':
