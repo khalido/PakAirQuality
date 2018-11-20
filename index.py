f6abe86193dd779a04eb53b5575a272312561c91
Khalid Omar <khalid.omar@gmail.com>
2018-11-20 13:30:24 +0000

    bug fix

--- index.py
+++ index.py
@@ -15,6 +15,6 @@ def hello_world():
     message = "<h1>This is a test</h1> <p>And this is a paragragh.</p>"
     
     if type(PAGE_ACCESS_TOKEN) == str:
-        msg += "<p>Page access token loaded<p>" + PAGE_ACCESS_TOKEN
+        message += "<p>Page access token loaded<p>" + PAGE_ACCESS_TOKEN
 
     return message + f"<p>Test Secret key is: {TEST_SECRET_KEY}</p> "
