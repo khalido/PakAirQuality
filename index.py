38c53240d0cf92a1542cf8ef2da922d317ffd68d
Khalid Omar <khalid.omar@gmail.com>
2018-11-20 13:29:10 +0000

    testing tokens

--- index.py
+++ index.py
@@ -9,7 +9,12 @@ FB_CLIENT_TOKEN = os.getenv("FB_CLIENT_TOKEN")
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 TEST_SECRET_KEY = os.getenv("TEST_SECRET_KEY")
 
+
 @app.route('/')
 def hello_world():
     message = "<h1>This is a test</h1> <p>And this is a paragragh.</p>"
+    
+    if type(PAGE_ACCESS_TOKEN) == str:
+        msg += "<p>Page access token loaded<p>" + PAGE_ACCESS_TOKEN
+
     return message + f"<p>Test Secret key is: {TEST_SECRET_KEY}</p> "
